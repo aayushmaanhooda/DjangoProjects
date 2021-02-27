ff43d55f1cb75fac7dc7ea0db264d4c0893baffa
Aayushmaan <aayushmaan162@gmail.com>
2021-02-27 19:48:56 +0530

    new files

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4hve1i!x9=_#8c3_2i@3ja%62z%+ceohvsrh3in4i#(+aapqks'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://git.heroku.com/todoapp002.git']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR ,"staticfiles")
